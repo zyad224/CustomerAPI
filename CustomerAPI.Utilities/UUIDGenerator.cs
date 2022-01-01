e523d3192341557948a2d4e034713487ce469140
Zeyad <zyabdelwahab1@sheffield.ac.uk>
2022-01-01 17:10:14 +0200

    Add UUID Generator Utility.

---
+++ CustomerAPI.Utilities/UUIDGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerAPI.Utilities
+{
+    public static class UUIDGenerator
+    {
+        public static string GetNewUUID()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}

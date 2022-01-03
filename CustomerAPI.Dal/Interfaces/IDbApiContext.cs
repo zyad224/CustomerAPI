f7dc7f470e532739ff3f43b1afb474132d7fb7fb
Zeyad <zyabdelwahab1@sheffield.ac.uk>
2022-01-03 21:21:26 +0200

    Adding IDbApiContext to api.

---
+++ CustomerAPI.Dal/Interfaces/IDbApiContext.cs
@@ -0,0 +1,15 @@
+﻿using CustomerAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerAPI.Dal.Interfaces
+{
+    public interface IDbApiContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
